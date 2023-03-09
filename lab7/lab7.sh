8c1a64d522d1148aade4c69502b672393f249a3f
xukum <xukum@localhost.localdomain>
2023-03-09 18:39:11 +0300

    lab7 v1

--- lab7/lab7.sh
+++ lab7/lab7.sh
@@ -5,15 +5,31 @@ err() {
 }
 
 printblockdevices() {
-  lsblk -o FSTYPE,MOUNTPOINT | awk -F'\t| {1,}' '{
-	if ($2 != "") {
-		system("df --output=source,target | grep $2/$ | cut -d \' \' -f1")
-		printf ("\t%s\t%s\n", $1, $2);
-  	}
-  }'
+	df --output=source,fstype,target -x proc -x sys -x tmpfs -x devtmpfs | grep -v Filesystem
 }
 
-printblockdevices
+readinput() {
+	local -n list=$1
+	list+=("Справка" "Назад")
+	select opt in "${list[@]}"; do
+	case $opt in
+		Назад) return 0;;
+		Справка) echo $2;;
+		*)
+			if [[ -z $opt ]]; then
+				echo "Ошибка: введите число из списка" >&2
+			else
+				return $REPLY
+			fi
+			;;
+	esac
+	done
+}
+
+if [ "$EUID" -ne 0 ]; then
+	echo "Не администратор"
+	exit
+fi
 
 PS3='Please enter your choice: '
 options=("Вывести таблицу файловых систем" "Монтировать файловую систему"\
@@ -26,73 +42,105 @@ options=("Вывести таблицу файловых систем" "Монт
 select opt in "${options[@]}"
 do
     case $opt in
-        "Вывести таблицу файловых систем")
-            echo "you chose choice 1"
-            lsblk --list | awk -F'\t| {1,}' '{
-		if ($7 != "")
-			printf ("%s\t%s\t%s\n", $1, $6, $7);
-	    }'
-            ;;
-
+	"Вывести таблицу файловых систем")
+		printblockdevices
+		;;
 	"Монтировать файловую систему")
-            read -p "Введите путь до файла/устройства: " filepath
-	    if [ ! -b $filepath ] && [ ! -f $filepath ]; then
-		err "not exist or not a blockdevice|file"
-		continue
-            fi
-
-	    read -p "Введите каталог монтирования: " mountpath
-
-	    if [ ! -e $mountpath ]; then
-		err "not exist"
-		mkdir $mountpath
-	    elif [ -d $mountpath ]; then
-		if [ -z "$(ls -A $mountpath)" ]; then
-			err "dir exists and empty"
-		else
-			err "dir exists and not empty"
-			continue
+        read -p "Введите путь до файла/устройства: " filepath
+		if [ "$filepath" == "" ]; then
+			err "filepath cant be empty"
+			continue 
 		fi
+	    if [ ! -b $filepath ] && [ ! -f $filepath ]; then
+			err "not exist or not a blockdevice|file"
+			continue
+        fi
+	    read -p "Введите каталог монтирования: " mountpath
+		if [ "$mountpath" == "" ]; then
+			err "mountpath cant be empty"
+			continue
+		fi 
+	    if [ ! -e $mountpath ]; then
+			mkdir -p $mountpath
+	    elif [ -d $mountpath ]; then
+			if [ ! -z "$(ls -A $mountpath)" ]; then
+				err "directory not empty"
+				continue
+			fi
 	    else
-		err "Not a directory"
-		continue
+			err "Not a directory"
+			continue
 	    fi
 
 	    if [ -f $filepath ]; then
-            	device=$(sudo losetup --find --show $filepath)
-	    	echo $device
-		#mount $device $mountpath
+            device=$(sudo losetup --find --show $filepath)
+			mount $device $mountpath
 	    else
-		echo ok
-            	#mount $filepath $mountpath
-            fi
+			mount $filepath $mountpath
+		fi
 	    ;;
 	"Отмонтировать файловую систему")
-            read -p "Введите путь до файловой системы: " filesyspath #считать с консоли или дать выбрать
-            umount $filesyspath
+		read -p "Введите путь до файловой системы: " filesyspath
+		if [ -z $filesyspath ]; then
+			readarray -t mounts < <(printblockdevices)
+			readinput mounts "Введите число, соответствующее опции"
+			res=$?
+			[ $res == 0 ] && continue
+			filesyspath=$(echo ${mounts[res - 1]} | cut -d " " -f3 )
+		fi
+		umount $filesyspath
+		check $? "Успешно отмонтировано" "Ошибка"
 	    ;;
 
-        "Изменить параметры монтирования примонтированной файловой системы")
-	    read -p "Введите путь до файловой системы: " remountfilesyspath #считать с консоли или дать выбрать
-	    read -p "1: только чтение\n2: чтение и запись\n" decision #сделать нормально
-	    #if 1
-	    mount -o remount,ro $remountfilesyspath
-	    #if 2
+    "Изменить параметры монтирования примонтированной файловой системы")
+	    read -p "Введите путь до файловой системы: " remountfilesyspath
+		if [ -z $remountfilesyspath ]; then
+			readarray -t mounts < <(printblockdevices)
+			readinput mounts "Введите число, соответствующее опции"
+			res=$?
+			[ $res == 0 ] && continue
+			remountfilesyspath=$(echo ${mounts[res - 1]} | cut -d " " -f3 )
+		fi
+
+	    chooseopt=("только чтение" "чтение и запись")
+		readinput chooseopt "Введите число, соответствующее интересующей опции"
+		res=$?
+		if [ $res == 1 ]; then
+	    	mount -o remount,ro $remountfilesyspath
+	    else
             mount -o remount,rw $remountfilesyspath
-            ;;
-        "Вывести параметры монтирования примонтированной файловой системы")
+		fi
+        ;;
+
+    "Вывести параметры монтирования примонтированной файловой системы")
 	    read -p "Введите путь до файловой системы: " filesyspath #считать с консоли или дать выбрать
+		if [ -z $filesyspath ]; then
+			readarray -t mounts < <(printblockdevices)
+			readinput mounts "Введите число, соответствующее папке, которую хотите отмонтировать"
+			res=$?
+			[ $res == 0 ] && continue
+			filesyspath=$(echo ${mounts[res - 1]} | cut -d " " -f3 )
+		fi
 	    mount | grep $filesyspath
-            ;;
+        ;;
  	"Вывести детальную информацию о файловой системе ext*")
-	    echo "Имеются следующие файловые системы ext*"
-	    df -Th -t ext2 -t ext3 -t ext4 -t extcow | awk -F'\t| {1,}' '{ printf ("%s\t%s\t%s\n", $1, $2, $7) }'
-	    read -p "Введите путь до файловой системы ext4: " ext4syspath #считать с консоли или дать выбрать
-	    tune2fs -l $ext4syspath
-            ;;
-        "Выйти")
-            break
-            ;;
+	    readarray -t exts < <(df -t ext2 -t ext3 -t ext4 -t extcow --output=source,fstype,target 2>&1)
+		arrsize=${#exts[@]}
+	    if [ $arrsize == 1 ]; then
+			echo "Нет файловых систем данного типа" 
+		else 
+			echo "Есть следующие системы:"
+			readarray -t exts < <(df -t ext2 -t ext3 -t ext4 -t extcow --output=source,fstype,target | grep -v Filesystem )
+			readinput exts "Введите число, соответствующее интересующей файловой системе"
+			res=$?
+			[ $res == 0 ] && continue
+			filesyspath=$(echo ${exts[res - 1]} | cut -d " " -f1 )
+			tune2fs -l $filesyspath
+		fi
+        ;;
+    "Выйти")
+        break
+        ;;
 	*) echo "invalid option $REPLY";;
     esac
 done
